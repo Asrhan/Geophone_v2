427b6ae0930f99cbde79569aa95ae91a3de4bb5e
Baptiste <freedom-gundam@hotmail.fr>
2016-12-18 21:47:04 +0100

    BLE : Modification Code Simple Recherche

--- app/src/main/java/com/example/flaforgue/geophone/managers/message/MessageCode.java
+++ app/src/main/java/com/example/flaforgue/geophone/managers/message/MessageCode.java
@@ -14,7 +14,7 @@ public final class MessageCode {
     public final static String SEND_LOCATION_FAR = "[GEOPHONE]SEND_LOCATION_FAR";
     public final static String UNKNOWN_LOCATION = "[GEOPHONE]UNKNOWN_LOCATION";
     public final static String ERROR_PIN = "[GEOPHONE]ERROR_PIN";
-    public final static String SIMPLE_LOCATION = "[GEOPHONE]SIMPLE_LOCATION";
+    public final static String SIMPLE_LOCATION = "[GEOPHONE]SIMPLE";
     public final static String MAPS_LINK = "[GEOPHONE]MAPS_LINK: ";
     public final static String MISSING_PIN = "[GEOPHONE]MISSING_PIN";
 
